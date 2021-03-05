0e4b9c6660f56d0332597ecc88c2b576f180f62f
YordiHernandez <80115849+YordiHernandez@users.noreply.github.com>
2021-03-05 11:49:29 -0600

    Ejercicios hoja trabajo 1 parcial 1

---
+++ lab1.py
@@ -0,0 +1,67 @@
+#print("LOS COMENTARIOS NO APARECEN EN LA EJECUCION DEL PROGRAMA XD")
+#como se puede ver los comentarios solo sirven como si fueran notas
+
+#print("ESTO ES UN STRING")
+#print('ESTO ES UN "STRING"')
+
+#letras=""" ESTO ES UN 'STRING'"""
+#print(letras)
+
+#variable="yordi"
+#print(type(variable))
+
+#string1="Mi nombre es:"
+#string2='Yordi'
+#string3=string1+string2
+
+#string4="Esto es "+"concatenar"
+#print(string4[0:4])
+
+#print("xd"*10)
+
+#string="En este texto hay 5 a mayuscula AAAAA"
+#print(string)
+#print(string.find("texto"))
+
+
+#string1='yordi'
+#string2="yordi"
+
+#print("Hola {} se te asignara el\n" 
+#"nombre de usuario {:>15}"
+#.format(string1,"SHIDER"))
+
+#import string
+#
+#plantilla=string.Template("EL JUEGO $GAME ES EL MEJOR\nDEL $year")
+
+#print(plantilla.substitute(GAME="God of war", year="2018"))
+
+#edad=int(input("Ingrese su edad: "))
+#fechanac=2021-edad
+#print("Su año de nacimiento es: ",fechanac)
+
+# altura=float(input("Ingrese su altura en cm: "))
+
+# print("Su altura es: ",altura)
+# print("El tipo de variable es: ",type(altura))
+
+# complex=99j
+# print("Numero complejo: ",complex)
+# print(type(complex))
+
+# print("Parte Real: ", complex.real)
+# print("Parte Compleja: ",complex.imag)
+
+#boleano=True
+#print("valor entero: ", int(boleano), "Tipo: ", type(boleano))
+
+# num1=2
+# num2=2
+
+# print("Suma entre {} y {}: ".format(num1,num2), num1+num2)
+# print("Division entre {} y {}: ".format(num1, num2), num2/num1)
+# print("Multiplicacion {} y {}: ".format(num1,num2),num1*num2)
+
+variablenula=None
+print("Valor de la variable nula y tipo: ",type(variablenula))
